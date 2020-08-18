79df64f089c8267a848ceb17ea71d61c4430358e
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-18 09:46:27 +0100

    Fake database added as a variable to test routes using Postman

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
 
+const database = {
+  user: [
+    {
+      id: 123,
+      name: "Chris Cooney",
+      email: "testing@test.co.uk",
+      password: "Password",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: 124,
+      name: "Laura Cooney",
+      email: "testing_Laura@test.co.uk",
+      password: "Password",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
   res.send("Testing");
+});
+
+app.post("/signin", (req, res) => {
+  res.json("Signing in");
 });
 
 app.listen(3000);
 
 /*
+/ --> possible redirect 
 /signin --> POST = success/reject
 /register --> POST = user object
 /profile/:id --> GET = user object
