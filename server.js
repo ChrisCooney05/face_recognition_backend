d775b7eb6bfdcb02a0530133536b5d244c78090f
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-19 14:50:10 +0100

    Updated register to save user and hashed password in db

--- server.js
+++ server.js
@@ -56,19 +56,31 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  db("users")
-    .returning("*")
-    .insert({
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
       email: email,
-      name: name,
-      joined: new Date(),
     })
-    .then(user => {
-      res.json(user[0]);
-    })
-    .catch(err => {
-      res.status(400).json("unable to register");
-    });
+      .into("login")
+      .returning("email")
+      .then(loginEmail => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then(user => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => {
+    res.status(400).json("unable to register");
+  });
 });
 
 app.get("/profile/:id", (req, res) => {
