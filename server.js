048024f7c3cb879500ce6ca335f97250013b4ca5
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-19 14:17:03 +0100

    profile/:id is linked to db, no use in siute currently

--- server.js
+++ server.js
@@ -73,16 +73,18 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.user.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("user not found");
-  }
+  db.select("*")
+    .from("users")
+    .where({
+      id: id,
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("user not found");
+      }
+    });
 });
 
 app.put("/image", (req, res) => {
