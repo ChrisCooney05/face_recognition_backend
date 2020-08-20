f554fe17f4d7669937610c4f355ce34c773c85cf
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-20 13:47:36 +0100

    Added validation to register and sign in to make sure there is data

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,28 +1,32 @@
 function handleSignIn(req, res, bcrypt, db) {
   const { email, password } = req.body
-  db.select('email', 'hash')
-    .from('login')
-    .where({
-      email: email
-    })
-    .then(data => {
-      const isValid = bcrypt.compareSync(password, data[0].hash)
-      if (isValid) {
-        db.select('*')
-          .from('users')
-          .where({
-            email: email
-          })
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => {
-            res.status(400).json('unable to get user')
-          })
-      } else {
-        res.status(400).json('wrong log in details')
-      }
-    })
+  if (!email || !password) {
+    res.status(400).json('incorrect log in details')
+  } else {
+    db.select('email', 'hash')
+      .from('login')
+      .where({
+        email: email
+      })
+      .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash)
+        if (isValid) {
+          db.select('*')
+            .from('users')
+            .where({
+              email: email
+            })
+            .then(user => {
+              res.json(user[0])
+            })
+            .catch(err => {
+              res.status(400).json('unable to get user')
+            })
+        } else {
+          res.status(400).json('wrong log in details')
+        }
+      })
+  }
 }
 
 module.exports = handleSignIn;
