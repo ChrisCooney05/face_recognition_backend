502060dbf1eed7841429bd5d11bee8d6d4632724
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-20 12:18:19 +0100

    Logic from register moved to own file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 const handleSignIn = require('./controllers/signIn')
+const handleRegistration = require('./controllers/register')
 
 const db = knex({
   client: "pg",
@@ -24,32 +25,7 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email,
-    })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date(),
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => {
-    res.status(400).json("unable to register");
-  });
+  handleRegistration(req, res, bcrypt, db)
 });
 
 app.get("/profile/:id", (req, res) => {
